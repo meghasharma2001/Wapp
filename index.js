797c97850679a9d529696827559d5433bba88772
Megha <msjpr2001@gmail.com>
2024-01-22 02:41:50 +0530

    8th

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const viewsPath = path.join(__dirname, "views");
 app.set('views', viewsPath);
-console.log(viewsPath)
+console.log(viewsPath);
 
-app.use(express.static("public"));
-
+const publicpath = path.join(__dirname,"public")
+app.use(express.static(publicpath));
+console.log(publicpath)
 app.set ("view engine" , "hbs");
 app.engine(".hbs" , engine({
     extname:".hbs",
